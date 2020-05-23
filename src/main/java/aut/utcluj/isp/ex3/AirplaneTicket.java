22746df796f315ece08203f93e14e95c11ad43a1
Maniu Pastiu <pastiumaniu@yahoo.com>
2020-05-23 17:48:25 +0300

    Exercise 3

--- src/main/java/aut/utcluj/isp/ex3/AirplaneTicket.java
+++ src/main/java/aut/utcluj/isp/ex3/AirplaneTicket.java
@@ -1,5 +1,7 @@
 package aut.utcluj.isp.ex3;
 
+import java.util.Objects;
+
 /**
  * @author stefan
  */
@@ -9,7 +11,12 @@ public class AirplaneTicket {
     private String destination;
 
     public AirplaneTicket(String id, Double price, String destination) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.id = id;
+        this.price = price;
+        this.destination = destination;
+    }
+
+    public AirplaneTicket() {
     }
 
     public String getId() {
@@ -22,5 +29,26 @@ public class AirplaneTicket {
 
     public String getDestination() {
         return destination;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AirplaneTicket)) return false;
+        AirplaneTicket ticket = (AirplaneTicket) o;
+        return Objects.equals(id, ticket.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
